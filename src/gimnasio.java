3bb071949697800537f40c5e6b27e41a743be39d
Pc <bmorenor@unbosque.edu.co>
2021-03-15 17:58:21 -0500

    Nuevo ejercicio b

---
+++ src/gimnasio.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @Author Brayan Camilo Moreno Romero
+ * Miguel Angel Sierra Morales
+ * Sergio Alejandro Hernandez Zambrano
+ */
+public class gimnasio {
+    public static int opcion;
+    public static List<Estudiantes>listaE;
+    public static Scanner leer;
+    public static String matriz [][];
+    public static int cubiculos;
+    public static int casilleros;
+    public static String nombre;
+    public static String apellido;
+    public static String cedula;
+    private static Estudiante estudiante;
+
+
+    public static void main(String[] args) {
+        listaE = new ArrayList<>();
+        leer = new Scanner(System.in);
+
+        cubiculos = leer.nextInt();
+
+        casilleros = leer.nextInt();
+        matriz= new String[cubiculos][casilleros];
+
+        opcion = leer.nextInt();
+        leer.nextLine();
+
+
+        for (int i = 0; i < cubiculos; i++) {
+            for (int j = 0; j < casilleros; j++) {
+                matriz[i][j]=""+(i+1)+","+(j+1)+"";
+
+            }
+        }
+
+
+        switch (opcion) {
+            case 1:
+                cedula = leer.nextLine();
+                String nom = leer.nextLine();
+
+                nombre = nom.split(" ")[0];
+                apellido = nom.split(" ")[1];
+//
+                estudiante = new Estudiante(nombre, apellido, cedula);
+
+                for(int i=0;i< matriz.length;i++){
+
+                }
+                for (int i = 0; i < cubiculos; i++) {
+                    for (int j = 0; j < casilleros; j++) {
+                        System.out.println(matriz[i][j]);
+
+                    }
+                }
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+                System.exit(0);
+                break;
+        }
+
+    }
+
+
+}
+
+class Estudiantes {
+    public String nombre;
+    public String apellido;
+    public String cedula;
+
+    public Estudiantes(String nombre, String apellido, String cedula) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+    }
+}
